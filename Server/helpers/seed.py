2ad01502cd5967140288e1237d176a7cb3b26dfe
dader34 <56900960+dader34@users.noreply.github.com>
2023-11-18 19:29:50 -0800

    Change seed.py file

--- Server/helpers/seed.py
+++ Server/helpers/seed.py
@@ -2,31 +2,17 @@
 
 import random
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from flask import Flask
 from faker import Faker
 import sys
 sys.path.append('.')
 #! Make sure to run this file from one directory up, or move the file up one directory or else it wont work !#
 from models import db, User, Post, Comment, Follower, PostLike, CommentLike
-
-# Create a Flask application to establish an application context
-app = Flask(__name__)
-
-# Specify the path to your SQLite database file
-database_path = 'instance/database.db'
-
-# Create the database engine and bind it to the Flask app
-engine = create_engine(f'sqlite:///{database_path}')
-db.metadata.create_all(engine)
-db.app = app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db.init_app(app)
+from main import app
 
 # Create a Flask application context
 with app.app_context():
-    Session = sessionmaker(bind=engine)
-    session = Session()
+
 
     fake = Faker()
 
@@ -35,36 +21,36 @@ with app.app_context():
 
     for _ in range(5):
         user = User(username=fake.pystr(min_chars=5, max_chars=10), password=fake.password())
-        session.add(user)
+        db.session.add(user)
         users.append(user)
 
-    session.commit()
+    db.session.commit()
 
     # Shuffle the users to ensure a random order
     random.shuffle(users)
 
     for i, user in enumerate(users):
         post = Post(user_id=user.id, content=fake.text())
-        session.add(post)
-        session.commit()  # Commit the post to get the post.id
+        db.session.add(post)
+        db.session.commit()  # Commit the post to get the post.id
 
         comment = Comment(user_id=user.id, post_id=post.id, content=fake.text())
-        session.add(comment)
-        session.commit()  # Commit the comment to get the comment.id
+        db.session.add(comment)
+        db.session.commit()  # Commit the comment to get the comment.id
 
         # Ensure that the follower and following are different users
         other_user = users[(i + 1) % len(users)]
 
         follower = Follower(follower_id=user.id, following_id=other_user.id, close_friend=random.choice([True, False]))
-        session.add(follower)
+        db.session.add(follower)
 
         post_like = PostLike(user_id=user.id, post_id=post.id)
-        session.add(post_like)
+        db.session.add(post_like)
 
         comment_like = CommentLike(user_id=user.id, comment_id=comment.id)
-        session.add(comment_like)
+        db.session.add(comment_like)
 
     # Commit the changes
-    session.commit()
+    db.session.commit()
 
     print("Seed data added successfully.")
