158af9c487c14c7128a015912c3f9446ce52fa44
dader34 <56900960+dader34@users.noreply.github.com>
2023-11-21 01:09:09 -0800

    add form validation for file input and bio

--- UI/src/auth/FileUpload.js
+++ UI/src/auth/FileUpload.js
@@ -1,52 +1,80 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const FileUpload = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [result, setResult] = useState(null);
-
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    setSelectedFile(file);
-  };
-
-  const uploadFile = () => {
-    if (selectedFile) {
-      const formData = new FormData();
-      formData.append('file', selectedFile);
-
-      fetch('https://backend.danner.repl.co/upload', {
-        method: 'POST',
-        body: formData,
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.blob_link)
-        setResult({
-          message: data.message,
-          blobLink: data.blob_link
-        });
-      })
-      .catch(error => {
-        setResult({ error: error.message });
-      });
-    } else {
-      alert('Please choose a file to upload.');
-    }
-  };
-
   return (
-    <div className="container mt-5">
-      <h2>File Upload Example</h2>
-      <input
-        type="file"
-        className="custom-file-input"
-        onChange={handleFileChange}
-        accept=".png, .heif, .jpg, .jpeg, .webp, .heic"
-      />
-      <button type="button" className="btn btn-primary" onClick={uploadFile}>
-        Upload File
-      </button>
-    </div>
+    <Formik
+      initialValues={{
+        file: '',
+        bio: '',
+      }}
+      validationSchema={Yup.object({
+        file: Yup.mixed()
+          .required('File is required')
+          .test(
+            'fileType',
+            'Invalid file type. Only images are allowed.',
+            (value) => {
+              if (!value || !value[0]) {
+                return true;
+              }
+
+              const allowedTypes = [
+                'image/jpeg',
+                'image/png',
+                'image/jpg',
+                'image/webp',
+                'image/heic',
+                'image/heif',
+              ];
+              return allowedTypes.includes(value[0].type);
+            }
+          ),
+        bio: Yup.string()
+          .min(1, 'Bio must be at least 1 character long')
+          .required('Bio is required'),
+      })}
+      onSubmit={async (values) => {
+        // Handle form submission logic here
+        console.log('Form values:', values);
+        const formData = new FormData();
+        formData.append('file', values.file[0]);
+
+        try {
+          const response = await fetch('https://backend.danner.repl.co/upload', {
+            method: 'POST',
+            body: formData,
+          });
+          const data = await response.json();
+          console.log(data);
+        } catch (error) {
+          console.error(error);
+        }
+      }}
+    >
+      <Form>
+        <h2>File Upload Example</h2>
+        <Field
+          type="file"
+          className="custom-file-input"
+          name="file"
+          accept=".png, .heif, .jpg, .jpeg, .webp, .heic"
+        />
+        {/* Handle validation errors here @ michael     vvvvvvvv     */}
+        <ErrorMessage name="file" render={(msg) => console.log(msg)} />
+
+        <Field
+          as="textarea"
+          style={{ resize: 'none' }}
+          name="bio"
+        />
+       {/* Handle validation errors here @ michael     vvvvvvvv     */}
+        <ErrorMessage name="bio" render={msg => console.log(msg)} />
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
 
