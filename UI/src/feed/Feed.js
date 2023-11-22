6d23ce2625e222cd25806d8cb541b6dfe4cc8339
dader34 <56900960+dader34@users.noreply.github.com>
2023-11-21 16:56:59 -0800

    Got posts to work

--- UI/src/feed/Feed.js
+++ UI/src/feed/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from '../Common/NavBar';
+import PostCard from './PostCard';
 // Import other necessary components and hooks
 
 const Feed = ({ user }) => {
@@ -7,9 +8,26 @@ const Feed = ({ user }) => {
 
   return (
     <>
-    {/* <NavBar /> */}
     <div className="feed">
       {/* List of Tweet components */}
+      <PostCard 
+      author={
+        {name:"Danner",
+        profile_picture:"https://merriam-webster.com/assets/mw/images/article/art-wap-landing-mp-lg/egg-3442-4c317615ec1fd800728672f2c168aca5@1x.jpg"
+        
+        }}
+      content={"Test tweet"}
+      likes={10}
+        />
+        <PostCard 
+      author={
+        {name:"Danner",
+        profile_picture:"https://merriam-webster.com/assets/mw/images/article/art-wap-landing-mp-lg/egg-3442-4c317615ec1fd800728672f2c168aca5@1x.jpg"
+        
+        }}
+      content={"Test tweet"}
+      likes={10}
+        />
     </div>
     </>
   );
